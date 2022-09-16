7bfe12241efa8fca565d1acd117d8ba6059e9877
Sudharsanan v <99385366+sudhar08@users.noreply.github.com>
2022-09-16 09:33:05 +0530

    Add files via upload

--- dynamic memory allocation.c
+++ dynamic memory allocation.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
-    int i,n;
-    float *element;
-	printf("\n\n Pointer : Find the largest element using Dynamic Memory Allocation :\n"); 
-	printf("-------------------------------------------------------------------------\n"); 	
-    printf(" Input total number of elements(1 to 100): ");
-    scanf("%d",&n);
-    element=(float*)calloc(n,sizeof(float));  // Memory is allocated for 'n' elements 
-    if(element==NULL)
-    {
-        printf(" No memory is allocated.");
-        exit(0);
-    }
-    printf("\n");
-    for(i=0;i<n;++i)  
-    {
-       printf(" Number %d: ",i+1);
-       scanf("%f",element+i);
-    }
-    for(i=1;i<n;++i)  
-    {
-       if(*element<*(element+i)) 
-           *element=*(element+i);
-    }
-    printf(" The Largest element is :  %.2f \n\n",*element);
-    return 0;
-}
 
+int main() {
+  int n,i;
+  double *data;
+  printf("Enter the total number of elements: ");
+  scanf("%d", &n);
+
+  // Allocating memory for n elements
+  data = (double *)calloc(n, sizeof(double));
+  if (data == NULL) {
+  printf("Error!!! memory not allocated.");
+  exit(0);
+  }
+
+  // Storing numbers entered by the user.
+  for(i = 0; i < n; ++i){
+  printf("Enter number%d: ", i + 1);
+  scanf("%lf", data + i);
+  }
+
+  // Finding the largest number
+  for (i = 1; i < n; ++i) {
+    if (*data < *(data + i)) {
+      *data = *(data + i);
+    }
+  }
+  printf("Largest number = %.2lf", *data);
+
+  free(data);
+
+  return 0;
+}
